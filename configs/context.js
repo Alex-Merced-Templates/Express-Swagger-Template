3586b01669c90ec350f08e3f89bb4ce6795bb07b
Alex Merced <alex@alexmerced.dev>
2021-08-05 23:55:37 -0400

    documentation

--- configs/context.js
+++ configs/context.js
@@ -4,11 +4,12 @@
 // use to deliver any data you'd like available to all
 // routes like models and other global data
 ////////////////////////////////////////
+import models from "../models/index.js";
 
 export default (req, res, next) => {
-    req.context = {
+  req.context = {
+    models,
+  };
 
-    }
-
-    next()
-}
+  next();
+};
